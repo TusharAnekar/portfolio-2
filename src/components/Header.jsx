22925a38f1a8dd7e7338fed83d1582977fb412c2
Tushar Anekar <anekar.tushar@gmail.com>
2024-01-30 11:31:55 +0530

    fix- updated latest resume link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -78,7 +78,7 @@ const Header = () => {
 
             <NavLink
               to={
-                "https://drive.google.com/file/d/1PTOyPCloO5U80hH_2sWSq2YClu8qGBrW/view?usp=sharing"
+                "https://drive.google.com/file/d/1SlE6Qrnb6oK5OYsoTl0TQBvl1vay6JQI/view?usp=sharing"
               }
               target="_blank"
               rel="noopener norefferrer"
